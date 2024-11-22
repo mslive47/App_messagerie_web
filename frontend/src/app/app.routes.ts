1e043ff9936f15fabc2cf59d17b152b61ae4354a
thierry <dollartiti35@gmail.com>
2024-11-22 18:03:47 -0500

    mise a jour de routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './login/pages/login-page/login-page.component';
 import { ChatPageComponent } from './chat/pages/chat-page/chat-page.component';
-
+import { loginPageGuard } from './guards/login-page.guard';
+import { chatPageGuard } from './guards/chat-page.guard';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginPageComponent },
-    { path: 'chat', component: ChatPageComponent },
-    { path: '**', component: LoginPageComponent }
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    canActivate: [loginPageGuard],
+  },
+  { path: 'chat', component: ChatPageComponent, canActivate: [chatPageGuard] },
+  { path: '**', component: LoginPageComponent, canActivate: [loginPageGuard] },
 ];
