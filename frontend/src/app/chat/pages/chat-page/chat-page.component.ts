b23f21068366bcaf7a67b4df6ba38553b28007b4
thierry <dollartiti35@gmail.com>
2024-11-23 18:54:34 -0500

    Ajout de la gestion de la session expiree

--- frontend/src/app/chat/pages/chat-page/chat-page.component.ts
+++ frontend/src/app/chat/pages/chat-page/chat-page.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 import { AuthenticationService } from 'src/app/login/services/authentication.service';
 import { Router } from '@angular/router';
 import { MessagesComponent } from '../../composants/messages/messages.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MessagesService } from 'src/app/chat/services/messages.service';
+import { NewMessageRequest } from '../../model/message.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat-page',
@@ -13,11 +16,28 @@ import { MatButtonModule } from '@angular/material/button';
   standalone: true,
   imports: [ReactiveFormsModule, MessagesComponent, MatButtonModule],
 })
-export class ChatPageComponent {
+export class ChatPageComponent implements OnInit, OnDestroy {
+  private logoutSubscription: Subscription = new Subscription(); // Valeur par défaut
+
   constructor(
     private authenticationService: AuthenticationService,
+    private messagesService: MessagesService,
     private router: Router
   ) {}
+
+  ngOnInit() {
+    // S'abonner à l'événement de déconnexion
+    this.logoutSubscription = this.messagesService.logoutEvent.subscribe(() => {
+      this.onLogout();
+    });
+  }
+
+  ngOnDestroy() {
+    // Se désabonner de l'événement lorsque le composant est détruit
+    if (this.logoutSubscription) {
+      this.logoutSubscription.unsubscribe();
+    }
+  }
 
   /** Cette méthode permet de faire la deconnexion du chat */
   async onLogout() {
