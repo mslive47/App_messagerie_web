76151b81872c10a02fdc21245bd8bcd39ef3ffc0
Salomon Mbungu <dallasman47@hotmail.com>
2024-11-28 04:30:44 -0500

    ajout test frontend

--- frontend/src/app/login/components/login-form/login-form.component.spec.ts
+++ frontend/src/app/login/components/login-form/login-form.component.spec.ts
@@ -4,23 +4,123 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { LoginFormComponent } from './login-form.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TestHelper } from 'src/app/test-helper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { By } from '@angular/platform-browser';
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
+  let testHelper: TestHelper<LoginFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       providers: [provideHttpClient(), provideHttpClientTesting()],
-      imports: [ReactiveFormsModule, LoginFormComponent, NoopAnimationsModule],
+      imports: [ReactiveFormsModule, LoginFormComponent, NoopAnimationsModule,  MatFormFieldModule,
+        MatInputModule,],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
+    testHelper = new TestHelper(fixture);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit username and password when form is valid', () => {
+    let emittedCredentials: { username: string; password: string } | undefined;
+
+    component.login.subscribe((event) => {
+      emittedCredentials = event;
+    });
+
+    const usernameInput = testHelper.getInput('username-input');
+    const passwordInput = testHelper.getInput('password-input');
+    const submitButton = testHelper.getButton('submit-button');
+
+    testHelper.writeInInput(usernameInput, 'James');
+    testHelper.writeInInput(passwordInput, 'abc');
+    fixture.detectChanges();
+
+    submitButton.click();
+
+    expect(emittedCredentials).toEqual({
+      username: 'James',
+      password: 'abc',
+    });
+    expect(component.loginForm.valid).toBe(true);
+  });
+
+  it('should show error if username is missing', () => {
+    const usernameInput = fixture.debugElement.query(By.css('[data-testid="username-input"]')).nativeElement;
+    const passwordInput = testHelper.getInput('password-input');
+    const submitButton = testHelper.getButton('submit-button');
+
+    //testHelper.writeInInput(usernameInput, '');
+    // Simulez un blur sans remplir le champ
+    usernameInput.value = ''; // Champ vide
+    usernameInput.dispatchEvent(new Event('input')); // Simule la saisie
+    usernameInput.dispatchEvent(new Event('blur'));  // Simule le blur
+    testHelper.writeInInput(passwordInput, 'abc');
+    fixture.detectChanges();
+
+    submitButton.click();
+
+    const usernameError = testHelper.getElement('username-error');
+    expect(usernameError).toBeTruthy();
+    expect(usernameError.textContent).toContain('nom requis');
+    expect(component.loginForm.valid).toBe(false);
+  });
+
+  it('should show error if password is missing', () => {
+    const passwordInput = fixture.debugElement.query(By.css('[data-testid="password-input"]')).nativeElement;
+    const usernameInput = testHelper.getInput('username-input');
+    const submitButton = testHelper.getButton('submit-button');
+
+    passwordInput.value = ''; // Champ vide
+    passwordInput.dispatchEvent(new Event('input')); // Simule la saisie
+    passwordInput.dispatchEvent(new Event('blur'));  // Simule le blur
+    testHelper.writeInInput(usernameInput, 'james');
+    fixture.detectChanges();
+
+    submitButton.click();
+
+    const passwordError = testHelper.getElement('password-error');
+    expect(passwordError).toBeTruthy();
+    expect(passwordError.textContent).toContain('mot de passe requis');
+    expect(component.loginForm.valid).toBe(false);
+  });
+
+  it('should show errors if both username and password are missing', () => {
+    const usernameInput = fixture.debugElement.query(By.css('[data-testid="username-input"]')).nativeElement;
+    const passwordInput = fixture.debugElement.query(By.css('[data-testid="password-input"]')).nativeElement;
+    const submitButton = testHelper.getButton('submit-button');
+
+    usernameInput.value = '';
+    usernameInput.dispatchEvent(new Event('input'));
+    usernameInput.dispatchEvent(new Event('blur'));
+    
+
+    passwordInput.value = '';
+    passwordInput.dispatchEvent(new Event('input'));
+    passwordInput.dispatchEvent(new Event('blur'));
+    fixture.detectChanges();
+
+    submitButton.click();
+
+    const usernameError = testHelper.getElement('username-error');
+    const passwordError = testHelper.getElement('password-error');
+
+    expect(usernameError).toBeTruthy();
+    expect(usernameError.textContent).toContain('nom requis');
+
+    expect(passwordError).toBeTruthy();
+    expect(passwordError.textContent).toContain('mot de passe requis');
+    expect(component.loginForm.valid).toBe(false);
+  });
+
 });
--- frontend/src/app/login/services/authentication.service.spec.ts
+++ frontend/src/app/login/services/authentication.service.spec.ts
@@ -1,10 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { AuthenticationService } from './authentication.service';
+import { environment } from 'src/environments/environment';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
+  let httpTestingController: HttpTestingController;
+
+  const loginData = {
+    username: 'username',
+    password: 'pwd',
+  };
+
+  afterEach(() => {
+    localStorage.clear();
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -16,4 +27,96 @@ describe('AuthenticationService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('on login', () => {
+    beforeEach(() => {
+      localStorage.clear();
+      httpTestingController = TestBed.inject(HttpTestingController);
+      //service = TestBed.inject(AuthenticationService);
+    });
+
+    it('should call POST with login data to auth/login', async () => {
+      const loginPromise = service.login(loginData);
+
+      const req = httpTestingController.expectOne(
+        `${environment.backendUrl}/auth/login`
+      );
+      expect(req.request.method).toBe('POST');
+      expect(req.request.body).toEqual(loginData);
+      req.flush({ username: loginData.username });
+
+      // wait for the login to complete
+      await loginPromise;
+    });
+
+    it('should store and emit the username', async () => {
+      // À compléter
+      const loginPromise = service.login(loginData);
+
+      const req = httpTestingController.expectOne(
+        `${environment.backendUrl}/auth/login`
+      );
+      req.flush(
+        { username: loginData.username },
+        { headers: { Authorization: 'Bearer test-jwt-token' } }
+      );
+
+      await loginPromise;
+
+      // Check localStorage values
+      expect(localStorage.getItem(AuthenticationService.KEY)).toBe(loginData.username);
+      expect(localStorage.getItem(AuthenticationService.TOKEN_KEY)).toBe('test-jwt-token');
+
+      // Check service signals
+      expect(service.getUsername()()).toBe(loginData.username);
+      expect(service.getToken()).toBe('test-jwt-token');
+
+    });
+  });
+
+  describe('on logout', () => {
+    beforeEach(() => {
+      localStorage.setItem('username', loginData.username);
+
+      httpTestingController = TestBed.inject(HttpTestingController);
+      //service = TestBed.inject(AuthenticationService);
+    });
+
+    it('should call POST with login data to auth/logout', async () => {
+      // À compléter
+      service['jwtToken'].set(' test-jwt-token');
+      const logoutPromise = service.logout();
+
+      const req = httpTestingController.expectOne(
+        `${environment.backendUrl}/auth/logout`
+      );
+      expect(req.request.method).toBe('POST');
+      expect(req.request.headers.get('Authorization')).toBe('Bearer test-jwt-token');
+      req.flush(null);
+
+      await logoutPromise;
+    });
+
+    it('should remove the username from the service and local storage', async () => {
+      // À compléter
+      const logoutPromise = service.logout();
+
+      const req = httpTestingController.expectOne(
+        `${environment.backendUrl}/auth/logout`
+      );
+      req.flush(null);
+
+      await logoutPromise;
+
+      // Check localStorage values
+      expect(localStorage.getItem(AuthenticationService.KEY)).toBeNull();
+      expect(localStorage.getItem(AuthenticationService.TOKEN_KEY)).toBeNull();
+
+      // Check service signals
+      expect(service.getUsername()()).toBeNull();
+      expect(service.getToken()).toBeNull();
+    });
+  });
+
+
 });
