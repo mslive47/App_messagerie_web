ebdfb77198cd802758b10c155153959a50cb52a1
thierry <dollartiti35@gmail.com>
2024-11-28 21:42:19 -0500

    MAJ FirestoreUserAccount.java

--- backend/src/main/java/com/inf5190/chat/auth/repository/FirestoreUserAccount.java
+++ backend/src/main/java/com/inf5190/chat/auth/repository/FirestoreUserAccount.java
@@ -1,4 +1,5 @@
 package com.inf5190.chat.auth.repository;
+import java.util.Objects;
 
 /**
  * Représente un message stocké dans firestore.
@@ -23,5 +24,27 @@ public class FirestoreUserAccount {
     }
     public void setEncodedPassword(String encodedPassword) {
         this.encodedPassword = encodedPassword;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FirestoreUserAccount that = (FirestoreUserAccount) o;
+        return Objects.equals(username, that.username) &&
+               Objects.equals(encodedPassword, that.encodedPassword);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, encodedPassword);
+    }
+
+    @Override
+    public String toString() {
+        return "FirestoreUserAccount{" +
+               "username='" + username + '\'' +
+               ", encodedPassword='" + encodedPassword + '\'' +
+               '}';
     }
 }
